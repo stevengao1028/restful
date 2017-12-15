7a13e779b5ff3bd445ed56e38de609acf31bf4c3
stevengao1028 <63297269@qq.com>
2017-12-15 15:00:23 +0800

    2017121515

--- apitest/views.py
+++ apitest/views.py
@@ -9,11 +9,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from apitest.models import profit,person
 from apitest.serializer import profitSerializer, personSerializer
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 def home(request):
     return render(request,'machine/index.html')
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'PUT'])
 # @csrf_exempt
 def profit_list(request):
     """
@@ -27,21 +29,16 @@ def profit_list(request):
         profits = profit.objects.all()
         serializer = profitSerializer(profits, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
-        print request.POST['searchtext']
-        # searchtext = request.data['searchtext']
-        # print searchtext
-        # serializer = profitSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        profits = profit.objects.all()
-        serializer = profitSerializer(profits, many=True)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'PUT':
+        # print request.data['stk_code']
+        serializer = profitSerializer(data=request.data)
+        print serializer
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET', 'POST'])
+@api_view(['GET','PUT'])
 def person_lists(request):
     """
     显示profit所有数据,后者创建一个新的snippet
@@ -50,9 +47,9 @@ def person_lists(request):
         persons = person.objects.all()
         serializer = personSerializer(persons, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    elif request.method == 'PUT':
         print str(request.data)
-        serializer = personSerializer(data=request.DATA)
+        serializer = personSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
